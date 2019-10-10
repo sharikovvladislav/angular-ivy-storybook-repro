cf300cb0e47cf2c522d2bc47d1266ee8db80c472
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2019-10-10 13:18:27 +0300

    storybook repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <span>woot</span>
+
+  `
 })
 export class AppComponent {
   title = 'with-ivy';
